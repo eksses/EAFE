5ac7ddb56db93abfbdea215d8498f409a2360537
eksses <shamirbhuiyan2@gmail.com>
2026-07-30 05:20:42 +0600

    fix: demo.js use public API instead of private props

--- examples/demo.js
+++ examples/demo.js
@@ -20,14 +20,17 @@ bot.once('spawn', () => {
 
   console.log('Bot spawned! Type "f X Z" in chat to fly.');
 
+  let targetX = 0;
+  let targetZ = 0;
+
   bot.on('chat', (user, msg) => {
     const args = msg.split(' ');
 
     if (args[0] === 'f') {
-      const x = parseInt(args[1]) || 0;
-      const z = parseInt(args[2]) || 0;
-      flight.fly(x, z);
-      bot.chat(`Flying to ${x} ${z}`);
+      targetX = parseInt(args[1]) || 0;
+      targetZ = parseInt(args[2]) || 0;
+      flight.fly(targetX, targetZ);
+      bot.chat(`Flying to ${targetX} ${targetZ}`);
     }
 
     if (args[0] === 'stop') {
@@ -40,7 +43,7 @@ bot.once('spawn', () => {
     if (args[0] === 'low') flight.setMode('LOW');
 
     if (args[0] === 'status') {
-      const s = flight.setStatus(flight._targetX, flight._targetZ);
+      const s = flight.setStatus(targetX, targetZ);
       bot.chat(`Phase: ${s.phase} | Pos: ${s.pos.x},${s.pos.y},${s.pos.z} | Dist: ${s.dist}m | Elytra: ${s.elytra.dur} | Rockets: ${s.rockets}`);
     }
   });
